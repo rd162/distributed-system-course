a6209b00f50856bcb4812b4c1981fe1e75c9b717
Dmitry Rudakov <drudakov@lohika.com>
2019-11-07 16:16:27 +0200

    Hello web service

---
+++ web-service/web/src/main/java/com/rd162/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.rd162.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public HelloResponse hello() {
+        return new HelloResponse();
+    }
+}
---
+++ web-service/web/src/main/java/com/rd162/web/HelloResponse.java
@@ -0,0 +1,13 @@
+package com.rd162.web;
+
+public class HelloResponse {
+    private String message = "Hello, World!";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
